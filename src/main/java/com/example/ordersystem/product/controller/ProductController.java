1b24dc985303ef4f1696ed27b80c03a46df4c01f
kimseonguk197 <ksg39412@naver.com>
2024-08-27 08:09:23 +0900

    fix

--- src/main/java/com/example/ordersystem/product/controller/ProductController.java
+++ src/main/java/com/example/ordersystem/product/controller/ProductController.java
@@ -42,7 +42,7 @@ public class ProductController {
 
     @GetMapping("/product/list")
     public String productList(ProductSearchDto searchDto, Pageable pageable){
-        return "hello world3";
+        return "hello world4";
     }
 
 
